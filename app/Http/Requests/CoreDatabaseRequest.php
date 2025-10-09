bff2041caf46532270a2468f3a2a86587b7bf7f5
vince0526 <vincentvillanueva@gmail.com>
2025-10-09 15:38:57 +0800

    chore(core): refine CREATE validation after manual edits

--- app/Http/Requests/CoreDatabaseRequest.php
+++ app/Http/Requests/CoreDatabaseRequest.php
@@ -1,4 +1,8 @@
-<?php
+git remote -vgit remote -vgit remote add origin <repo-url>
+# or
+git remote set-url origin <repo-url>git remote add origin <repo-url>
+# or
+git remote set-url origin <repo-url><?php
 
 declare(strict_types=1);
 
